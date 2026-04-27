03cdb9204a1a243f4fcb02aaa80ec620adac53b1
DadeWillms <Dadewillms@gmail.com>
2026-04-27 13:58:01 -0500

    Remove oyher stuff

--- tests/RemoveEmptyLines.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('midterm_knn.csv')
-
-df_cleaned = df.dropna(subset=['StudyHours','SleepHours','AttendancePercent','AssignmentScore','ScreenTimeHours','ExerciseHours','SocialMediaHours','ProjectsCompleted','PartTimeJobHours','ExamScore'])
-
-df_cleaned.to_csv('cleaned_file.csv', index=False)
-
-
